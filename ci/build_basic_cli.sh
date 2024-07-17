8137603cf586f967369cce0426a82ae0ee0778dc
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2024-07-17 21:09:57 +0200

    only sed on linux
    Signed-off-by: Anton-4 <17049058+Anton-4@users.noreply.github.com>

--- ci/build_basic_cli.sh
+++ ci/build_basic_cli.sh
@@ -44,8 +44,10 @@ cd ..
 roc version
 
 cd basic-cli
-sed -i.bak 's|target/release|target/x86_64-unknown-linux-musl/release|g' jump-start.sh
-sed -i.bak 's|target/release|target/x86_64-unknown-linux-musl/release|g' build.roc
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sed -i 's|target/release|target/x86_64-unknown-linux-musl/release|g' jump-start.sh
+    sed -i 's|target/release|target/x86_64-unknown-linux-musl/release|g' build.roc
+fi
 ./jump-start.sh
 
 # build the basic cli platform
